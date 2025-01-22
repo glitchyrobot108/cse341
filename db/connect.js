381abdfd825fe2c34fea4116d2b52cf34da0344b
GlitchyRobot <glitchyrobot998@gmail.com>
2025-01-21 23:20:39 -0800

    added rest

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const mongoose = require("mongoose")
 
 const connectDB =  () => {
-     mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+     mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     console.log("Connected to DB")
 }
 
