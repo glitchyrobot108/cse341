c38a9801acb3a82cae5e6ac19974dbce2b2ac89f
GlitchyRobot <glitchyrobot998@gmail.com>
2025-02-02 15:49:54 -0800

    swagger

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ const express = require("express")
 require('dotenv').config()
 const connectDB = require("./db/connect")
 const app = express()
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+const cors = require("cors")
+const corsOptions = {
+    origin: "*", // Replace with your allowed origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204
+};
 
 //Connect MongoDB
 connectDB()
 
 //Middleware
 app.use(express.json())
+.use(cors(corsOptions));
 
 //Routs Handling
 app.use("/contacts", require("./routes/contacts"))
+
+//Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 //Initial base route
 app.get("/", (req, res) => {
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,40 +1,89 @@
-const Contact = require("../models/contact")
+const Contact = require("../models/contact");
 
-async function getContacts(req, res){
-        const contacts = await Contact.find({})
-        res.json(contacts)
-};
- 
-async function getContact(req, res){
-        const contact = await Contact.find({_id:req.params.id})
-        res.json(contact)
-};
+async function getContacts(req, res) {
+    try {
+        const contacts = await Contact.find({});
+        res.json(contacts);
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
 
-async function createContact(req, res){
+async function getContact(req, res) {
+    try {
+        const contact = await Contact.findById(req.params.id);
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+        res.json(contact);
+    } catch (error) {
+        if (error.name === "CastError") {
+            res.status(404).json({ message: "Contact not found" });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+}
+
+async function createContact(req, res) {
+    try {
         const contact = await Contact.create({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                favoriteColor: req.body.favoriteColor,
-                birthday: req.body.birthday
-        })
-        res.status(201).send(contact)
-}
-    
-async function updateContact(req, res){
-        const contact = await Contact.findByIdAndUpdate({_id: req.params.id}, {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                favoriteColor: req.body.favoriteColor,
-                birthday: req.body.birthday
-        }, { new: true })
-        res.status(204).json(contact)
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        });
+        res.status(201).json(contact);
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            res.status(400).json({ message: error.message, errors: error.errors });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
 }
 
-async function deleteContact(req, res){
-        const contact = await Contact.deleteOne({_id: req.params.id})
-        res.status(200).json(contact)
+async function updateContact(req, res) {
+    try {
+        const contact = await Contact.findByIdAndUpdate(
+            req.params.id,
+            {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                favoriteColor: req.body.favoriteColor,
+                birthday: req.body.birthday
+            },
+            { new: true }
+        );
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+        res.status(200).json(contact);
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            res.status(400).json({ message: error.message, errors: error.errors });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+}
+
+async function deleteContact(req, res) {
+    try {
+        const contact = await Contact.deleteOne({ _id: req.params.id });
+        if (!contact.deletedCount) {
+            return res.status(404).json({ message: "Contact not found" });
+        }
+        res.status(200).json({ message: "Contact deleted successfully" });
+    } catch (error) {
+        if (error.name === "CastError") {
+            res.status(404).json({ message: "Contact not found" });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
 }
 
 module.exports = {
@@ -43,4 +92,4 @@ module.exports = {
     createContact,
     updateContact,
     deleteContact
-}
+};
