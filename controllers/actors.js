0d54859d6eb7c85c3ff02b29cfbc4f2c8714c542
GlitchyRobot <glitchyrobot998@gmail.com>
2025-02-17 20:22:18 -0800

    Added functions for both actor and movie controllers

--- controllers/actors.js
+++ controllers/actors.js
@@ -9,18 +9,39 @@ async function getActors(req, res) {
     }
 }
 
+async function getActorById(req, res) {
+    try {
+        const actor = await Actor.find({_id: req.params.id})
+        res.json(actor)
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+async function getActorByName(req, res) {
+    try {
+        const actor = await Actor.find({
+            firstName: req.params.firstName,
+            lastName: req.params.lastName
+        })
+        res.json(actor)
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
 async function createActor(req, res) {
     try {
-        const { firstName, lastName, birthdate, country, stars_in } = req.body;
+        const { firstName, lastName, birthdate, country, movies } = req.body;
 
-        const Actor = await Actor.create({
+        const actor = await Actor.create({
             firstName,
             lastName,
             birthdate,
             country,
-            stars_in
+            movies
         });
-        res.status(201).json(Actor);
+        res.status(201).json(actor);
     } catch (error) {
         if (error.name === "ValidationError") {
             res.status(400).json({ message: error.message, errors: error.errors });
@@ -30,7 +51,53 @@ async function createActor(req, res) {
     }
 }
 
+async function updateActor(req, res) {
+    try {
+        const actor = await actor.findByIdAndUpdate(
+            req.params.id,
+            {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                birthdate: req.body.birthdate,
+                country: req.body.country,
+                movies: req.body.movies
+            },
+            { new: true }
+        );
+        if (!actor) {
+            return res.status(404).json({ message: "actor not found" });
+        }
+        res.status(200).json(actor);
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            res.status(400).json({ message: error.message, errors: error.errors });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+}
+
+async function deleteActor(req, res) {
+    try {
+        const actor = await actor.deleteOne({ _id: req.params.id });
+        if (!actor.deletedCount) {
+            return res.status(404).json({ message: "actor not found" });
+        }
+        res.status(200).json({ message: "actor deleted successfully" });
+    } catch (error) {
+        if (error.name === "CastError") {
+            res.status(404).json({ message: "actor not found" });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+}
+
 module.exports = {
     getActors,
-    createActor
+    getActorById,
+    getActorByName,
+    createActor,
+    updateActor,
+    deleteActor
 }
--- controllers/movies.js
+++ controllers/movies.js
@@ -9,6 +9,26 @@ async function getMovies(req, res) {
     }
 }
 
+async function getMovieById(req, res) {
+    try {
+        const movie = await Movie.find({_id: req.params.id})
+        res.json(movie)
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+async function getMovieByTitle(req, res) {
+    try {
+        const movie = await Movie.find({
+            title: req.params.title
+        })
+        res.json(movie)
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
 async function createMovie(req, res) {
     try {
         const movie = await Movie.create({
@@ -17,7 +37,7 @@ async function createMovie(req, res) {
             genre: req.body.genre,
             rating: req.body.rating,
             parentRating: req.body.parentRating,
-            starring: req.body.starring,
+            actors: req.body.actors,
             director: req.body.director
         })
         res.status(201).json(movie)
@@ -30,7 +50,53 @@ async function createMovie(req, res) {
     }
 }
 
+async function updateMovie(req, res) {
+    try {
+        const movie = await Movie.findByIdAndUpdate(
+            req.params.id,
+            {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                birthdate: req.body.birthdate,
+                country: req.body.country,
+                movies: req.body.movies
+            },
+            { new: true }
+        );
+        if (!movie) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+        res.status(200).json(Movie);
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            res.status(400).json({ message: error.message, errors: error.errors });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+}
+
+async function deleteMovie(req, res) {
+    try {
+        const movie = await Movie.deleteOne({ _id: req.params.id });
+        if (!movie.deletedCount) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+        res.status(200).json({ message: "Movie deleted successfully" });
+    } catch (error) {
+        if (error.name === "CastError") {
+            res.status(404).json({ message: "Movie not found" });
+        } else {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+}
+
 module.exports = {
     getMovies,
-    createMovie
+    getMovieById,
+    getMovieByTitle,
+    createMovie,
+    updateMovie,
+    deleteMovie
 }
